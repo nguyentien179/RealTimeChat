350b8e12c796c8212b2e3b176aad9d560498ccc4
Hung Bui <hung.buiquang@nashtechglobal.com>
2025-06-08 22:43:41 +0700

    Refactor code

--- messaging/Program.cs
+++ messaging/Program.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using messaging.Application;
@@ -10,12 +9,12 @@ using messaging.Infrastructure;
 using messaging.Infrastructure.Extension;
 using messaging.Infrastructure.Repositories;
 using messaging.Infrastructure.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -48,15 +47,13 @@ builder.Services.AddSingleton<IUserIdProvider, CustomUserIdProvider>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(
-        "AllowFrontendAccess",
-        builder =>
-            builder
-                .WithOrigins("http://localhost:5173")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials()
-                .WithExposedHeaders("WWW-Authenticate")
+    var frontendUrl = builder.Configuration.GetValue<string>("FrontendUrl");
+    options.AddPolicy("AllowFrontendAccess", builder => builder
+        .WithOrigins(!string.IsNullOrEmpty(frontendUrl) ? frontendUrl : "http://localhost:5173")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .WithExposedHeaders("WWW-Authenticate")
     );
 });
 
@@ -99,16 +96,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-// Swagger UI
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Messaging API v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Messaging API v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontendAccess");
